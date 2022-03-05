e3cf317d853b10ea45d79c7adbf383507674dcea
kjudom <kjudom@gmail.com>
2022-03-05 09:02:24 +0700

    update version 0.0.7

--- src/tsp_c/__init__.py
+++ src/tsp_c/__init__.py
@@ -1,8 +1,1 @@
-"""
-``tsp_c``
-=========================
-"""
-
-from .tsp import solve_greedy  # noqa
-from .tsp import solve_SA  # noqa
-from .tsp import set_param_SA  # noqa
+from . import _tsp_c
